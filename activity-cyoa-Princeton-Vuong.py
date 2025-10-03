aba5dca9f9072100199a7aaf963ef07d64c3c0a8
Princeton <princeton.vuong@gmail.com>
2025-10-03 09:20:54 -0700

    First commit - added CYOA and notes

---
+++ activity-cyoa-Princeton-Vuong.py
@@ -0,0 +1,85 @@
+# Choose Your own adventure
+# Princeton Vuong
+# 24 September
+
+# Choose your own adventure story focusing on using
+# variable and branching/conditionals
+
+import sys
+
+# Intro
+greeting = "Welcome to the diddy dungeon"
+print(greeting)
+
+health = 0
+inventory = []
+
+name = input("What shall I call you traveller?")
+print(f"Nice to meet you {name}!")
+
+print("Please select a class from the following options.")
+print("mage")
+print("assassin")
+print("warrior")
+player_class = input("Which class would you like to select?")
+
+if player_class == "mage":
+    print(f"You chose {player_class}, a long distance class with exceptional power")
+    health = 60
+    inventory.append("staff")
+elif player_class == "assassin":
+    print(f"You chose {player_class}, a close distance class with exceptional speed")
+    health = 75
+    inventory.append("dagger")
+elif player_class == "warrior":
+    print(f"You chose {player_class}, a close distance class with exceptional defence")
+    health = 100
+    inventory.append("shield")
+
+if health > 0:
+    print(f"{name} the {player_class}, has {health} health points and a {inventory}.")
+
+if health <= 0:
+    print("GAME OVER!")
+
+startgame = input("Do you wish to begin? Yes/No")
+
+if startgame == "yes":
+    print("Now the story begins.")
+    print("With a Dragon who is terrorizing a small village")
+    print("This Dragon resides in the great volcano far away.")
+    print(
+        "Your job,as the village's last hope,is to travel to the great volcano and slay this Dragon once and for all."
+    )
+    print("Your journey begins in the village tavern.")
+
+else:
+    print("restart the game dumbshit")
+    sys.exit()
+# Problem
+print("To help you on your journey, we will give you one free potion of your choosing.")
+print("speed")
+print("health")
+print("invis")
+drink1 = input("select one of the options.")
+
+if drink1 == "speedpot":
+    inventory.append("speedpot")
+    print(f"You have picked up a {drink1}")
+
+elif drink1 == "healthpot":
+    inventory.append("healthpot")
+    print(f"You have picked up a {drink1}")
+
+elif drink1 == "invispot":
+    inventory.append("invispot")
+    print(f"You have picked up a {drink1}")
+
+print("As you were about to leave the tavern, a monster appears")
+
+# Rising action
+
+# Climax
+
+# Resolution
+#
---
+++ notes-1-intro.py
@@ -0,0 +1,41 @@
+# Notes - Intoduction
+# 16 September
+# Princeton Vuong
+
+# Create out own Chatbot
+
+# 1. Greet the user with a predetermined statement
+greeting = "Hello! I am a Chatbot."
+
+# 2. Print out the statement
+print(greeting)
+
+# 3. Introduce the bot
+print("My name is MeGPT")
+print("Im not like the other Chatbots.")
+print("Im completely deterministic")
+
+# 4. Wow the user with some maths
+print("I bet you dont know what 8x8 is.")
+print("I can do it")
+print(f"8x8 is actually {8*8}.")
+
+print("What is pi squared?")
+print("I'm smart and I an do it")
+print(f"It is {3.141592653 ** 2}")
+
+#5. Get the name of the user and store it
+user_name = input("So... what's your name? ")
+print(f"It's nice to meet you, {user_name}.")
+
+videogame = input("What's your favorite videogame?")
+print(f"I also like {videogame}, I find it very fun.")
+
+sport = input("What kind of sports do you play?")
+print(f"Interesting, I find {sport} to be a bit too hard for me.")
+
+shower = input("How many times shower per day?")
+print(f"You only shower {shower} times per day!? You must smell bad.")
+
+
+print(f"Goodbye, {user_name}")
