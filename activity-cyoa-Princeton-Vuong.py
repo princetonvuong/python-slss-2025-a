cdd14f39758baa3ca01595245cefbe00963addb5
Princeton <princeton.vuong@gmail.com>
2025-10-07 14:22:44 -0700

    CYOA changes

--- activity-cyoa-Princeton-Vuong.py
+++ activity-cyoa-Princeton-Vuong.py
@@ -23,15 +23,15 @@ print("assassin")
 print("warrior")
 player_class = input("Which class would you like to select?")
 
-if player_class == "mage":
+if player_class.lower() == "mage":
     print(f"You chose {player_class}, a long distance class with exceptional power")
     health = 60
     inventory.append("staff")
-elif player_class == "assassin":
+elif player_class.lower() == "assassin":
     print(f"You chose {player_class}, a close distance class with exceptional speed")
     health = 75
     inventory.append("dagger")
-elif player_class == "warrior":
+elif player_class.lower() == "warrior":
     print(f"You chose {player_class}, a close distance class with exceptional defence")
     health = 100
     inventory.append("shield")
@@ -44,7 +44,7 @@ if health <= 0:
 
 startgame = input("Do you wish to begin? Yes/No")
 
-if startgame == "yes":
+if startgame.lower() == "yes":
     print("Now the story begins.")
     print("With a Dragon who is terrorizing a small village")
     print("This Dragon resides in the great volcano far away.")
@@ -54,7 +54,7 @@ if startgame == "yes":
     print("Your journey begins in the village tavern.")
 
 else:
-    print("restart the game dumbshit")
+    print("restart the game dummy")
     sys.exit()
 # Problem
 print("To help you on your journey, we will give you one free potion of your choosing.")
@@ -63,23 +63,30 @@ print("health")
 print("invis")
 drink1 = input("select one of the options.")
 
-if drink1 == "speedpot":
+if drink1.lower() == "speedpot":
     inventory.append("speedpot")
     print(f"You have picked up a {drink1}")
 
-elif drink1 == "healthpot":
+elif drink1.lower() == "healthpot":
     inventory.append("healthpot")
     print(f"You have picked up a {drink1}")
 
-elif drink1 == "invispot":
+elif drink1.lower() == "invispot":
     inventory.append("invispot")
     print(f"You have picked up a {drink1}")
 
-print("As you were about to leave the tavern, a monster appears")
-
 # Rising action
+print("As you were about to leave the tavern, an ogre appears")
+print("fight")
+print("run")
+print("hide")
+choice1 = input("Do you wish to do?")
+enemy_health = 50
+if choice1 == "fight":
+    print(f"You have encountered an ogre with {enemy_health} health")
+    if player_class == "mage":
+
 
 # Climax
 
 # Resolution
-#
